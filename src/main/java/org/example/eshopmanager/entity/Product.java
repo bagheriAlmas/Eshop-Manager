fc4583bd9442cf281150b6675f6b87064fcc62e3
mahdibagheri <bagheri.almas@gmail.com>
2025-11-14 09:19:09 +0330

    Create product model

---
+++ src/main/java/org/example/eshopmanager/entity/Product.java
@@ -0,0 +1,31 @@
+package org.example.eshopmanager.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "stock", nullable = false)
+    private Integer stock = 100;
+
+    @Version
+    private Integer version;
+}
