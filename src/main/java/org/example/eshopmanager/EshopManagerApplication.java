0354868c6df535d4c62493aa67763b08b49bc507
mahdibagheri <bagheri.almas@gmail.com>
2025-11-14 09:08:02 +0330

    first commit

---
+++ src/main/java/org/example/eshopmanager/EshopManagerApplication.java
@@ -0,0 +1,13 @@
+package org.example.eshopmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopManagerApplication.class, args);
+    }
+
+}
