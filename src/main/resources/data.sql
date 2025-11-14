e9bcb4c7e5f851c8d94249e0b45b4c53829147df
mahdibagheri <bagheri.almas@gmail.com>
2025-11-14 09:40:43 +0330

    Add test data in H2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO products (name, price, stock, version)
+VALUES ('iPhone 15', 999.99, 100, 0);
+
+INSERT INTO products (name, price, stock, version)
+VALUES ('MacBook Pro 14"', 2499.00, 100, 0);
+
+INSERT INTO products (name, price, stock, version)
+VALUES ('AirPods Pro 2', 249.99, 100, 0);
+
+INSERT INTO products (name, price, stock, version)
+VALUES ('Apple Watch Ultra 2', 799.99, 100, 0);
