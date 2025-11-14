5ec0e323e1f942bba6a4732be42d8b1f70aef5fb
mahdibagheri <bagheri.almas@gmail.com>
2025-11-14 10:54:05 +0330

    Update test data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO products (name, price, stock, version)
-VALUES ('iPhone 15', 999.99, 100, 0);
+INSERT INTO products (name, price, stock, reserved_stock, reservation_expiry, version)
+VALUES ('iPhone 15', 999.99, 100, 0, NULL, 0);
 
-INSERT INTO products (name, price, stock, version)
-VALUES ('MacBook Pro 14"', 2499.00, 100, 0);
+INSERT INTO products (name, price, stock, reserved_stock, reservation_expiry, version)
+VALUES ('MacBook Pro 14"', 2499.00, 100, 0, NULL, 0);
 
-INSERT INTO products (name, price, stock, version)
-VALUES ('AirPods Pro 2', 249.99, 100, 0);
+INSERT INTO products (name, price, stock, reserved_stock, reservation_expiry, version)
+VALUES ('AirPods Pro 2', 249.99, 100, 0, NULL, 0);
 
-INSERT INTO products (name, price, stock, version)
-VALUES ('Apple Watch Ultra 2', 799.99, 100, 0);
+INSERT INTO products (name, price, stock, reserved_stock, reservation_expiry, version)
+VALUES ('Apple Watch Ultra 2', 799.99, 100, 0, NULL, 0);
